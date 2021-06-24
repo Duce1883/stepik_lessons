b57d04aab4c9d5f24a6b81c84267747510fc9868
Artiom Liapunov <duce>
2021-06-24 10:57:30 +0300

    fix module 5 Product Page links

--- module_5/test_product_page.py
+++ module_5/test_product_page.py
@@ -7,8 +7,9 @@ from .pages.product_page import ProductPage
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
-
 class TestProductPage():
+    prod_page_link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+
     @pytest.mark.xfail
     def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser):
         page = ProductPage(browser, link)
@@ -24,14 +25,12 @@ class TestProductPage():
         page.should_disappeared_success_message()
 
     def test_guest_should_see_login_link_on_product_page(self, browser):
-        prod_link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-        page = ProductPage(browser, prod_link)
+        page = ProductPage(browser, TestProductPage.prod_page_link)
         page.open()
         page.should_be_login_link()
 
     def test_guest_can_go_to_login_page_from_product_page(self, browser):
-        prod_link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-        page = ProductPage(browser, prod_link)
+        page = ProductPage(browser, TestProductPage.prod_page_link)
         page.open()
         page.should_be_login_link()
         page.go_to_login_page()
